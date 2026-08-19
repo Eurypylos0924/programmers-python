4929e037344a0c471452c6f7a605005f1aa9769c
Eurypylos0924 <eurypylos0924@gmail.com>
2026-08-19 16:16:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.09 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ Python/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,21 @@
+def solution(num):
+    count = 0
+    answer = 0
+    if num == 1:
+        answer
+    else:
+        while num != 1:
+            if num%2 == 0:
+                num = num/2
+                count += 1
+                answer += 1
+            else:
+                num = num*3 + 1
+                count += 1
+                answer += 1
+                
+            if count == 500:
+                answer = -1
+                break
+                
+    return answer
