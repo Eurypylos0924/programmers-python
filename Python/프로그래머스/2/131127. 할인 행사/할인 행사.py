fd64dde7bbdfed8c2e8dd68600697494c2e393e5
Eurypylos0924 <eurypylos0924@gmail.com>
2026-08-26 11:40:04 +0900

    [level 2] Title: 할인 행사, Time: 18.13 ms, Memory: 18.6 MB -BaekjoonHub

--- Python/프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ Python/프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,13 +1,29 @@
 def solution(want, number, discount):
     answer = 0
     shopdict = dict(zip(want,number))
-    for i in range(0,len(discount)-9):
-        cri = {}
-        for x in range(i,i+10):
-            if discount[x] in cri:
-                cri[discount[x]] += 1
-            else:
-                cri[discount[x]] = 1
+    l = len(discount)
+
+    cri = {}
+    for x in range(0,10):
+        if discount[x] in cri:
+            cri[discount[x]] += 1
+        else:
+            cri[discount[x]] = 1
+            
+    if cri == shopdict:
+        answer += 1
+        
+    for i in range(1,l-9):
+        cri[discount[i-1]] -= 1
+        if cri[discount[i-1]] == 0:
+            del cri[discount[i-1]]
+            
+        if discount[i+9] in cri:
+            cri[discount[i+9]] += 1
+        else:
+            cri[discount[i+9]] = 1
+        
         if cri == shopdict:
             answer += 1
+    
     return answer
