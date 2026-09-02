cf88ad56e7c19b042256d0f04f36d45535b6318d
Eurypylos0924 <eurypylos0924@gmail.com>
2026-09-02 11:46:42 +0900

    [level 2] Title: 피로도, Time: 16.99 ms, Memory: 11.4 MB -BaekjoonHub

--- Python/프로그래머스/2/87946. 피로도/피로도.py
+++ Python/프로그래머스/2/87946. 피로도/피로도.py
@@ -1,30 +1,17 @@
-def count_d(k,order):
-    fatigue = k
-    count = 0
-    for di in order:
-        minfat = di[0]
-        usage = di[1]
-        if fatigue >= minfat:
-            fatigue -= usage
-            count += 1
-    return count    
- 
- 
-
 def solution(k, dungeons):
     l = len(dungeons)
     visited = [False]*l
     answer = [0]
- 
+    
     def dfs(fatigue, count):
         answer[0] = max(answer[0], count)
- 
+        
         for i in range(l):
             if not visited[i] and fatigue >= dungeons[i][0]:
                 visited[i] = True
                 dfs(fatigue - dungeons[i][1], count + 1)
                 visited[i] = False
- 
+                
     dfs(k, 0)
- 
+    
     return answer[0]
