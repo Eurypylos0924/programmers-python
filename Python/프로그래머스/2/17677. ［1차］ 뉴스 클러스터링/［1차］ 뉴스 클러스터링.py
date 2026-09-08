aa4125792cdec150f438fa3d1840dce948d7ab37
Eurypylos0924 <eurypylos0924@gmail.com>
2026-09-08 15:19:30 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.56 ms, Memory: 11.7 MB -BaekjoonHub

--- Python/프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ Python/프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,18 +1,13 @@
 from collections import Counter
 
 def solution(str1, str2):
-    answer = 0
+    def to_counter(s):
+        return Counter([s[i:i+2].lower() for i in range(len(s)-1) if s[i:i+2].isalpha()])
     
-    dict1 = Counter([(str1[i]+str1[i+1]).lower() for i in range(0,len(str1)-1) if (str1[i]+str1[i+1]).isalpha()])
-    dict2 = Counter([(str2[i]+str2[i+1]).lower() for i in range(0,len(str2)-1) if (str2[i]+str2[i+1]).isalpha()])
+    dict1, dict2 = to_counter(str1), to_counter(str2)
+    a_sum = sum((dict1 & dict2).values())
+    b_sum = sum((dict1 | dict2).values())
     
-    a = dict1 & dict2   # 교집합
-    b = dict1 | dict2   # 합집합
-    a_sum = sum(list(a.values()))
-    b_sum = sum(list(b.values()))
-    
-    if dict1 == dict2:
+    if b_sum == 0:
         return 65536
-    else:
-        return ((a_sum/b_sum)*65536)//1   
-    
+    return int((a_sum / b_sum) * 65536)
