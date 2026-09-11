c64a89c44cbd665df214b45490e269244370c694
Eurypylos0924 <eurypylos0924@gmail.com>
2026-09-11 16:20:16 +0900

    [level 2] Title: 방문 길이, Time: 0.27 ms, Memory: 11.5 MB -BaekjoonHub

--- Python/프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ Python/프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,15 +1,15 @@
 def solution(dirs):
-    answer = 0
-    spot = (0,0)
-    direction = {'U':(0,1),'D':(0,-1),'L':(-1,0),'R':(1,0)}
-    line = []
+    spot = (0, 0)
+    direction = {'U':(0,1), 'D':(0,-1), 'L':(-1,0), 'R':(1,0)}
+    visited = []
     
-    for i in range(len(dirs)):
-        if abs(spot[0] + direction[dirs[i]][0]) < 6 and abs(spot[1] + direction[dirs[i]][1]) < 6:
-            nspot = (spot[0] + direction[dirs[i]][0], spot[1] + direction[dirs[i]][1])
-            line.append(frozenset([spot,nspot]))
+    for c in dirs:
+        dx, dy = direction[c]
+        nx, ny = spot[0] + dx, spot[1] + dy
+        
+        if abs(nx) < 6 and abs(ny) < 6:
+            nspot = (nx, ny)
+            visited.append(frozenset([spot, nspot]))
             spot = nspot
-        else:
-            continue
-
-    return len(set(line))
+    
+    return len(set(visited))
